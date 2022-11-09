31a6022185c1648f4b0aa29b5e3dbe89e653e041
Anjum1998 <78790254+Anjum1998@users.noreply.github.com>
2022-11-09 19:46:11 +0530

    Update TestMedicine.java

--- TestMedicine.java
+++ TestMedicine.java
@@ -41,38 +41,12 @@ class Ointment extends Medicine {
 class TestMedicine
 {
     public static void main(String[] args) {
-      //  Medicine m[]=new Medicine[10];
-              /*  m.displayLabel("syrup","abc","hhh");
-                Random random=new Random();
-                int x= random.nextInt(4);
-                System.out.println("randomly generated integers value");
-                System.out.println(x);*/
+    
         double i=Math.random()*4;
         System.out.println(i);
         int j=(int)i;
         System.out.println(j);
-       /* switch (j)
-        {
-            case 1:
-                m[0]=new Medicine();
-                m[1]=new Tablet();
-                m[0].displayLabel("dolo","microlabs","banglore");
-                m[1].displayLabel("dolo","microlabs","banglore");
-                break;
-            case 2:
-                m[2]=new Medicine();
-                m[3]=new Syrup();
-                m[2].displayLabel("dry cough syrup","Medico remedies","mumbai");
-                m[3].displayLabel("dry cough syrup","Medico remedies","mumbai");
-            case 3:
-                m[4]=new Medicine();
-                m[5]=new Ointment();
-                m[4].displayLabel("bacitracin","safetec","America");
-                m[5].displayLabel("bacitracin","safetec","America");
-                break;
-            default:
-                System.out.println("Invalid");
-        }*/
+      
         switch (j)
         {
             case 1:
